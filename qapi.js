b4842fd72d7273469574b367b68fac99a070b8d9
Peregrinus <24322711+onloocker@users.noreply.github.com>
2020-12-29 00:41:34 +0300

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fea5047a6aceb001f320490",
+				"5fea5047a6aceb001f320492"
+			]
+		},
+		"5fea5047a6aceb001f320490": {
+			"id": "5fea5047a6aceb001f320490",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fea5047a6aceb001f320492": {
+			"id": "5fea5047a6aceb001f320492",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
